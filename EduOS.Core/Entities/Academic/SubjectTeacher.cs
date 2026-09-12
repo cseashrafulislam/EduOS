9fad6ea2a0132d2b653cd442f55e4891251f4706
Md. Ashraful Islam <cseashrafulislam@gmail.com>
2026-09-12 22:41:15 +0600

    fix: normalize subject teacher foreign keys

--- EduOS.Core/Entities/Academic/SubjectTeacher.cs
+++ EduOS.Core/Entities/Academic/SubjectTeacher.cs
@@ -5,11 +5,11 @@ namespace EduOS.Core.Entities.Academic
 {
     public class SubjectTeacher : BaseTenantEntity
     {
-        public int AcademicYearId { get; set; }
-        public int ClassId { get; set; }
-        public int SectionId { get; set; }
-        public int SubjectId { get; set; }
-        public int TeacherId { get; set; }
+        public long AcademicYearId { get; set; }
+        public long ClassId { get; set; }
+        public long SectionId { get; set; }
+        public long SubjectId { get; set; }
+        public long TeacherId { get; set; }
         public bool IsClassTeacher { get; set; } = false;
 
         public virtual AcademicYear? AcademicYear { get; set; }
