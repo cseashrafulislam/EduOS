f2c68c04502cc4e2b5e0f90e625e782f41c25d29
Md. Ashraful Islam <cseashrafulislam@gmail.com>
2026-09-13 03:39:15 +0600

    fix: normalize course relation identifiers

--- EduOS.Core/Entities/LMS/Course.cs
+++ EduOS.Core/Entities/LMS/Course.cs
@@ -7,10 +7,10 @@ namespace EduOS.Core.Entities.LMS
     public class Course : BaseTenantEntity
     {
         public Guid PublicId { get; set; } = Guid.NewGuid();
-        public int AcademicYearId { get; set; }
-        public int ClassId { get; set; }
-        public int? SectionId { get; set; }
-        public int SubjectId { get; set; }
+        public long AcademicYearId { get; set; }
+        public long ClassId { get; set; }
+        public long? SectionId { get; set; }
+        public long SubjectId { get; set; }
         public string Title { get; set; } = string.Empty;
         public string? Description { get; set; }
         public long TeacherId { get; set; }
