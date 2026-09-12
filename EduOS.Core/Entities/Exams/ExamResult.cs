9a2a2c26bc841e8eafc4b42e0665419017f36dd8
Md. Ashraful Islam <cseashrafulislam@gmail.com>
2026-09-12 11:08:15 +0600

    feat: add immutable exam result academic snapshots

--- EduOS.Core/Entities/Exams/ExamResult.cs
+++ EduOS.Core/Entities/Exams/ExamResult.cs
@@ -7,11 +7,20 @@ namespace EduOS.Core.Entities.Exams
     {
         public int ExamId { get; set; }
         public int StudentId { get; set; }
+        public int AcademicYearId { get; set; }
+        public int ClassId { get; set; }
+        public int SectionId { get; set; }
+        public int? GroupId { get; set; }
         public decimal TotalMark { get; set; }
+        public decimal TotalFullMark { get; set; }
+        public decimal Percentage { get; set; }
         public decimal TotalGPA { get; set; }
         public string? FinalGrade { get; set; }
         public int? Position { get; set; }
         public bool IsPassed { get; set; } = true;
+        public bool IsPublished { get; set; }
+        public DateTime? PublishedAtUtc { get; set; }
+        public long? PublishedByUserId { get; set; }
 
         public virtual Exam? Exam { get; set; }
         public virtual Student? Student { get; set; }
