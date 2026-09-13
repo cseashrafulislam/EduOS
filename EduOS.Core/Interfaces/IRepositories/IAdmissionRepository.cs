c9cd695e8160b853420cd67e17d60d2157ea88aa
Md. Ashraful Islam <cseashrafulislam@gmail.com>
2026-09-13 13:44:23 +0600

    fix: normalize legacy admission relations

--- EduOS.Core/Interfaces/IRepositories/IAdmissionRepository.cs
+++ EduOS.Core/Interfaces/IRepositories/IAdmissionRepository.cs
@@ -5,9 +5,9 @@ namespace EduOS.Core.Interfaces.IRepositories
     public interface IAdmissionRepository : IGenericRepository<Admission>
     {
         Task<Admission?> GetByApplicationNoAsync(string appNo);
-        Task<List<Admission>> GetByStatusAsync(string status, int tenantId);
-        Task<List<Admission>> GetByYearAsync(int academicYearId);
-        Task<string> GenerateApplicationNoAsync(int tenantId, int academicYearId);
-        Task<int> GetCountByStatusAsync(string status, int tenantId);
+        Task<List<Admission>> GetByStatusAsync(string status, long tenantId);
+        Task<List<Admission>> GetByYearAsync(long academicYearId);
+        Task<string> GenerateApplicationNoAsync(long tenantId, long academicYearId);
+        Task<int> GetCountByStatusAsync(string status, long tenantId);
     }
 }
