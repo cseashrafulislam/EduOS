faf9c4feadc8410aa1f25caa72f884e49c7e8e85
Md. Ashraful Islam <cseashrafulislam@gmail.com>
2026-09-13 06:47:51 +0600

    fix: align admit card identifiers to long

--- EduOS.Core/Entities/Exams/AdmitCard.cs
+++ EduOS.Core/Entities/Exams/AdmitCard.cs
@@ -5,8 +5,8 @@ namespace EduOS.Core.Entities.Exams
 {
     public class AdmitCard : BaseTenantEntity
     {
-        public int ExamId { get; set; }
-        public int StudentId { get; set; }
+        public long ExamId { get; set; }
+        public long StudentId { get; set; }
         public string AdmitNo { get; set; } = string.Empty;
         public DateTime IssueDate { get; set; }
         public bool IsIssued { get; set; } = false;
