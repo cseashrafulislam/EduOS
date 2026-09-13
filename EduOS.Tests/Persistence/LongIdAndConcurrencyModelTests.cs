114de76630fcffc8c38c91f873291884440392e9
Md. Ashraful Islam <cseashrafulislam@gmail.com>
2026-09-14 05:40:34 +0600

    test: lock normalized long-id model contracts

---
+++ EduOS.Tests/Persistence/LongIdAndConcurrencyModelTests.cs
@@ -0,0 +1,54 @@
+using EduOS.Core.Entities.Academic;
+using EduOS.Core.Entities.Attendance;
+using EduOS.Core.Entities.SaaS;
+using EduOS.Core.Entities.Transport;
+using EduOS.Persistence.Context;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace EduOS.Tests.Persistence;
+
+public class LongIdAndConcurrencyModelTests
+{
+    [Fact]
+    public void Normalized_models_use_long_foreign_keys_without_legacy_shadow_id1_properties()
+    {
+        using var context = CreateContext();
+
+        AssertLongProperties<ClassRoutine>(context, nameof(ClassRoutine.AcademicYearId), nameof(ClassRoutine.ClassId), nameof(ClassRoutine.SectionId), nameof(ClassRoutine.SubjectId), nameof(ClassRoutine.TeacherId));
+        AssertLongProperties<StudentTransport>(context, nameof(StudentTransport.StudentId), nameof(StudentTransport.VehicleId), nameof(StudentTransport.RouteId));
+        AssertLongProperties<LeaveApplication>(context, nameof(LeaveApplication.UserId), nameof(LeaveApplication.LeaveTypeId));
+        AssertLongProperties<TrialAccount>(context, nameof(TrialAccount.TenantId));
+
+        var trial = context.Model.FindEntityType(typeof(TrialAccount))!;
+        trial.FindProperty(nameof(TrialAccount.ConvertedToPlanId))!.ClrType.Should().Be(typeof(long?));
+
+        foreach (var entityType in new[] { typeof(ClassRoutine), typeof(StudentTransport), typeof(LeaveApplication), typeof(TrialAccount) })
+        {
+            var entity = context.Model.FindEntityType(entityType)!;
+            entity.GetProperties().Should().NotContain(p => p.PropertyInfo == null && p.FieldInfo == null && p.Name.EndsWith("Id1", StringComparison.Ordinal));
+        }
+    }
+
+    [Fact]
+    public void Subscription_and_transport_write_models_keep_optimistic_concurrency_tokens()
+    {
+        using var context = CreateContext();
+        var subscription = context.Model.FindEntityType(typeof(TenantSubscription))!;
+        var transport = context.Model.FindEntityType(typeof(StudentTransport))!;
+
+        subscription.FindProperty(nameof(TenantSubscription.RowVersion))!.IsConcurrencyToken.Should().BeTrue();
+        transport.FindProperty(nameof(StudentTransport.RowVersion))!.IsConcurrencyToken.Should().BeTrue();
+    }
+
+    private static EduOSDbContext CreateContext() => new(new DbContextOptionsBuilder<EduOSDbContext>()
+        .UseInMemoryDatabase($"long-id-model-{Guid.NewGuid():N}").Options);
+
+    private static void AssertLongProperties<TEntity>(EduOSDbContext context, params string[] propertyNames)
+    {
+        var entity = context.Model.FindEntityType(typeof(TEntity))!;
+        foreach (var propertyName in propertyNames)
+            entity.FindProperty(propertyName)!.ClrType.Should().Be(typeof(long), $"{typeof(TEntity).Name}.{propertyName} is a normalized foreign-key boundary");
+    }
+}
