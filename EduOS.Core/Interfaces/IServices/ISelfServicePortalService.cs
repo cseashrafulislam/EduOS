b13f517ded709e704fd5d5cafb869d4be11e98bf
Md. Ashraful Islam <cseashrafulislam@gmail.com>
2026-09-17 00:43:21 +0600

    feat: add portal transport service contract

--- EduOS.Core/Interfaces/IServices/ISelfServicePortalService.cs
+++ EduOS.Core/Interfaces/IServices/ISelfServicePortalService.cs
@@ -9,4 +9,5 @@ public interface ISelfServicePortalService
     Task<ApiResponse<IReadOnlyList<PortalAttendanceDto>>> GetAttendanceAsync(Guid studentReference, DateTime fromDate, DateTime toDate, CancellationToken cancellationToken = default);
     Task<ApiResponse<IReadOnlyList<PortalResultDto>>> GetResultsAsync(Guid studentReference, CancellationToken cancellationToken = default);
     Task<ApiResponse<PortalFeeLedgerDto>> GetFeesAsync(Guid studentReference, CancellationToken cancellationToken = default);
+    Task<ApiResponse<IReadOnlyList<PortalTransportDto>>> GetTransportAsync(Guid studentReference, CancellationToken cancellationToken = default);
 }
