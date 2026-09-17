10077080373e359a26d419713c1a1ec3d9f3eac5
Md. Ashraful Islam <cseashrafulislam@gmail.com>
2026-09-17 21:43:32 +0600

    security: rate limit library mutations

--- EduOS.App/Controllers/Api/LibraryController.cs
+++ EduOS.App/Controllers/Api/LibraryController.cs
@@ -3,6 +3,7 @@ using EduOS.Core.DTOs.Library;
 using EduOS.Core.Interfaces.IServices;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
 
 namespace EduOS.App.Controllers.Api;
 
@@ -23,9 +24,11 @@ public sealed class LibraryController : ControllerBase
     public async Task<IActionResult> MyIssues(CancellationToken ct) => ToAction(await _service.GetMyIssuesAsync(ct));
 
     [HttpPost("issues")]
+    [EnableRateLimiting("ApiPolicy")]
     public async Task<IActionResult> Issue([FromBody] IssueBookDto request, CancellationToken ct) => ToAction(await _service.IssueAsync(request, ct));
 
     [HttpPost("issues/{reference:guid}/close")]
+    [EnableRateLimiting("ApiPolicy")]
     public async Task<IActionResult> Close(Guid reference, [FromBody] ReturnBookDto request, CancellationToken ct) => ToAction(await _service.CloseAsync(reference, request, ct));
 
     private IActionResult ToAction<T>(EduOS.Core.Common.ApiResponse<T> response) => StatusCode(response.StatusCode, response);
