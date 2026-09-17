10661ebd5ef73e4e30c744fd52e13054fdb1c511
Md. Ashraful Islam <cseashrafulislam@gmail.com>
2026-09-17 21:43:43 +0600

    security: rate limit transport mutations

--- EduOS.App/Controllers/Api/TransportController.cs
+++ EduOS.App/Controllers/Api/TransportController.cs
@@ -3,6 +3,7 @@ using EduOS.Core.DTOs.Transport;
 using EduOS.Core.Interfaces.IServices;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
 
 namespace EduOS.App.Controllers.Api;
 
@@ -26,9 +27,11 @@ public sealed class TransportController : ControllerBase
     public async Task<IActionResult> MyAssignment(CancellationToken ct) => ToAction(await _service.GetMyAssignmentAsync(ct));
 
     [HttpPost("assignments")]
+    [EnableRateLimiting("ApiPolicy")]
     public async Task<IActionResult> Assign([FromBody] AssignTransportDto request, CancellationToken ct) => ToAction(await _service.AssignAsync(request, ct));
 
     [HttpPost("assignments/{reference:guid}/close")]
+    [EnableRateLimiting("ApiPolicy")]
     public async Task<IActionResult> Close(Guid reference, [FromBody] CloseTransportDto request, CancellationToken ct) => ToAction(await _service.CloseAsync(reference, request, ct));
 
     private IActionResult ToAction<T>(EduOS.Core.Common.ApiResponse<T> response) => StatusCode(response.StatusCode, response);
