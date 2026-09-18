8ece0bc882a37e5608243e277867e94d18921e49
Md. Ashraful Islam <cseashrafulislam@gmail.com>
2026-09-18 12:42:52 +0600

    security: rate limit tenant settings workflow

--- EduOS.App/Controllers/Api/TenantSettingController.cs
+++ EduOS.App/Controllers/Api/TenantSettingController.cs
@@ -2,11 +2,13 @@ using EduOS.Core.DTOs.Tenants;
 using EduOS.Core.Interfaces.IServices;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
 
 namespace EduOS.App.Controllers.Api
 {
     [Authorize(Roles = "TenantAdmin,SuperAdmin")]
     [AutoValidateAntiforgeryToken]
+    [EnableRateLimiting("ApiPolicy")]
     [ApiController]
     [Route("api/tenant-settings")]
     public class TenantSettingController : ControllerBase
