a04e14dcfbb02c4a59af99f97a1ce6819ecddbe4
Md. Ashraful Islam <cseashrafulislam@gmail.com>
2026-09-20 05:43:56 +0600

    test: scope payroll id contracts to actual relations

--- EduOS.Tests/PayrollIdContractTests.cs
+++ EduOS.Tests/PayrollIdContractTests.cs
@@ -16,7 +16,6 @@ public sealed class PayrollIdContractTests
     [InlineData(typeof(Increment), nameof(Increment.Id), typeof(long))]
     [InlineData(typeof(Increment), nameof(Increment.TenantId), typeof(long))]
     [InlineData(typeof(Increment), nameof(Increment.EmployeeId), typeof(long))]
-    [InlineData(typeof(Increment), nameof(Increment.ApprovedBy), typeof(long))]
     [InlineData(typeof(LoanAdvance), nameof(LoanAdvance.Id), typeof(long))]
     [InlineData(typeof(LoanAdvance), nameof(LoanAdvance.TenantId), typeof(long))]
     [InlineData(typeof(LoanAdvance), nameof(LoanAdvance.EmployeeId), typeof(long))]
