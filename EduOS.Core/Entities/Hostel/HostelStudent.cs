33ad5b1b8054479210b3856650db0c8a3045cad3
Md. Ashraful Islam <cseashrafulislam@gmail.com>
2026-09-19 12:41:12 +0600

    fix: normalize legacy hostel student ids

--- EduOS.Core/Entities/Hostel/HostelStudent.cs
+++ EduOS.Core/Entities/Hostel/HostelStudent.cs
@@ -1,12 +1,11 @@
-﻿using EduOS.Core.Entities.Base;
 using EduOS.Core.Entities.Base;
 
 namespace EduOS.Core.Entities.Hostel
 {
     public class HostelStudent : BaseTenantEntity
     {
-        public int StudentId { get; set; }
-        public int HostelRoomId { get; set; }
+        public long StudentId { get; set; }
+        public long HostelRoomId { get; set; }
         public DateTime JoinDate { get; set; }
         public decimal MonthlyRent { get; set; }
     }
