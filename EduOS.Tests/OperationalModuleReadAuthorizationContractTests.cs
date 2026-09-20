a1335564c6d644e2f87f2e01ea0700ab417f8a10
Md. Ashraful Islam <cseashrafulislam@gmail.com>
2026-09-20 08:44:29 +0600

    fix: align operational authorization contracts with endpoints

--- EduOS.Tests/OperationalModuleReadAuthorizationContractTests.cs
+++ EduOS.Tests/OperationalModuleReadAuthorizationContractTests.cs
@@ -7,8 +7,11 @@ namespace EduOS.Tests;
 public sealed class OperationalModuleReadAuthorizationContractTests
 {
     [Theory]
-    [InlineData(typeof(LibraryController), nameof(LibraryController.Index))]
-    [InlineData(typeof(TransportController), nameof(TransportController.Index))]
+    [InlineData(typeof(LibraryController), nameof(LibraryController.Catalog))]
+    [InlineData(typeof(LibraryController), nameof(LibraryController.MyIssues))]
+    [InlineData(typeof(TransportController), nameof(TransportController.Routes))]
+    [InlineData(typeof(TransportController), nameof(TransportController.Vehicles))]
+    [InlineData(typeof(TransportController), nameof(TransportController.MyAssignment))]
     public void Operational_module_reads_inherit_authenticated_controller_boundary(Type controllerType, string actionName)
     {
         Assert.NotEmpty(controllerType.GetCustomAttributes(typeof(AuthorizeAttribute), true));
