3f833995fce43bc809f07d26e80d6ec51093e873
Md. Ashraful Islam <cseashrafulislam@gmail.com>
2026-09-21 00:45:01 +0600

    fix: normalize legacy student fee identifier

--- EduOS.Core/Entities/Finance/StudentFee.cs
+++ EduOS.Core/Entities/Finance/StudentFee.cs
@@ -4,7 +4,7 @@ namespace EduOS.Core.Entities.Finance
 {
     public class StudentFee : BaseTenantEntity
     {
-        public int StudentId { get; set; }
+        public long StudentId { get; set; }
         public int FeeTypeId { get; set; }
         public decimal Amount { get; set; }
         public DateTime DueDate { get; set; }
