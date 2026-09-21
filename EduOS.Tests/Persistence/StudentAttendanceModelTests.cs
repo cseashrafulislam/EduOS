344e1071b437c599b4ad86464d74c4dd6130dde2
Md. Ashraful Islam <cseashrafulislam@gmail.com>
2026-09-22 00:21:50 +0600

    test(attendance): read provider-neutral column metadata

--- EduOS.Tests/Persistence/StudentAttendanceModelTests.cs
+++ EduOS.Tests/Persistence/StudentAttendanceModelTests.cs
@@ -16,7 +16,7 @@ public sealed class StudentAttendanceModelTests
         using var context = new EduOSDbContext(new DbContextOptionsBuilder<EduOSDbContext>().UseInMemoryDatabase($"attendance-model-{Guid.NewGuid():N}").Options);
         var entity = context.GetService<IDesignTimeModel>().Model.FindEntityType(typeof(StudentAttendance))!;
 
-        entity.FindProperty(nameof(StudentAttendance.Date))!.GetColumnType().Should().Be("date");
+        entity.FindProperty(nameof(StudentAttendance.Date))!.FindAnnotation("Relational:ColumnType")!.Value.Should().Be("date");
         entity.FindProperty(nameof(StudentAttendance.Status))!.GetMaxLength().Should().Be(20);
         entity.GetIndexes().Single(x => x.GetDatabaseName() == "UX_StudentAttendances_Tenant_Student_Date").IsUnique.Should().BeTrue();
         entity.GetIndexes().Should().ContainSingle(x => x.GetDatabaseName() == "IX_StudentAttendances_Tenant_Roster_Date");
